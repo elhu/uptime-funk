1377f7b707347265bd0bcd1104a08c8c43210f35
Vincent Boisard <vincent.boisard@dimelo.com>
2016-01-09 11:38:59 +0100

    better: add index to outage boundaries

--- db/migrate/20160108194951_switch_outages_to_tstzranges.rb
+++ db/migrate/20160108194951_switch_outages_to_tstzranges.rb
@@ -1,9 +1,9 @@
 class SwitchOutagesToTstzranges < ActiveRecord::Migration
   def change
-    add_column :outages, :boundaries, :tstzrange
+    add_column :outages, :boundaries, :tstzrange, index: true
 
     Outage.all.each do |outage|
-      outage.update_attributes!(boundaries: (outage.attributes['started_at'])..(outage.attributes['finished_at'].presence || 1000.years.from_now))
+      outage.update_attributes!(boundaries: (outage.attributes['started_at'])..(outage.attributes['finished_at'].presence || Outage.default_end_time))
     end
     remove_column :outages, :started_at
     remove_column :outages, :finished_at
