416a9424a436a82e4751609c2c8a7beaa809d41a
Vincent Boisard <vincent.boisard@dimelo.com>
2016-03-03 15:36:51 +0100

    feature: display outages per day

--- TODO.md
+++ TODO.md
@@ -1,3 +1,10 @@
 # TODO:
 
+## Backend:
 * Switch Outage#boundaries index to GiST
+
+## Style:
+
+* Metro icons alignments for following width ranges:
+  * 384px-413px
+  * 498px-667px
--- app/assets/javascripts/components/uptime.js.jsx
+++ app/assets/javascripts/components/uptime.js.jsx
@@ -1,14 +1,21 @@
 var Uptime = React.createClass({
   propTypes: {
     status: React.PropTypes.string.isRequired,
-    date: React.PropTypes.string.isRequired
+    date: React.PropTypes.number.isRequired
   },
 
+  // days: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+  days: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+
   render: function() {
+    var date = new Date();
+    date.setTime(this.props.date * 1000);
+    var classes = ["uptime", this.props.status, this.days[date.getDay()].toLowerCase()];
     return (
-      <div className={"uptime " + this.props.status}>
-        <div className="uptime-tick-border" />
-        <UptimeTick label={this.props.date} />
+      <div className={classes.join(" ")}>
+        <div className="day">
+          {this.days[date.getDay()][0]}
+        </div>
       </div>
     );
   }
